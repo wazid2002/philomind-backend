634d31ed9ebd00a426e62bdb04dd88b83c77a0de
Wazid <munavalliwazid@gmail.com>
2025-03-15 15:47:44 +0530

    authentication fixes

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./src/config/db');
-const {clerkRequireAuth}= require("@clerk/express")
+const {requireAuth,createClerkClient}= require("@clerk/express");
+const {Clerk} = require('@clerk/clerk-sdk-node');
 
 dotenv.config();
 const app = express();
+const clerkClient = createClerkClient({ secretKey: process.env.CLERK_SECRET_KEY });
+
 
 // Middleware
 app.use(express.json());
@@ -12,7 +15,7 @@ app.use(express.json());
 // Connect MongoDB
 connectDB();
 
-app.get("/",clerkRequireAuth(),async(req,res)=>{
+app.get("/",requireAuth(),async(req,res)=>{
     try{
         const getUsers = await clerkClient.users.getUserList();
         res.json(getUsers)
