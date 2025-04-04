05ff7588803824b095a51b86e001b33c88cffb38
Wazid <munavalliwazid@gmail.com>
2025-04-04 15:49:21 +0530

    add cors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const connectDB = require('./src/config/db');
 const lessonRoutes = require("./src/routes/lessonRoutes");
 const profileRoutes= require("./src/routes/profileRoutes")
+const cors = require("cors")
 
 dotenv.config();
 const app = express();
@@ -10,6 +11,7 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(cors());
 
 // Connect MongoDB
 connectDB();
