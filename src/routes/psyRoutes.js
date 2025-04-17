7971400f1755c50b81321428115e3bd7aab06792
Wazid <munavalliwazid@gmail.com>
2025-04-17 16:02:50 +0530

    add filter and pagination

--- src/routes/psyRoutes.js
+++ src/routes/psyRoutes.js
@@ -4,8 +4,25 @@ const Psychiatrist = require("../models/Psychiatrist");
 
 router.get('/psy',async(req,res)=>{
 try{
-    const hospitals= await Psychiatrist.find();
-    res.status(200).json(hospitals);
+    const { city, page=1 , limit = 10 } = req.query;
+
+    const filter = city ? {city}:{};
+
+    const skip = (page - 1)*limit;
+    const hospitals= await Psychiatrist.find(filter)
+    .skip(skip)
+    .limit(Number(limit));
+
+    const totalCount= await Psychiatrist.countDocuments(filter);
+    const totalPages = Math.ceil(totalCount / limit);
+
+    res.status(200).json({
+        hospitals,
+        totalCount,
+        totalPages,
+        currentPage:Number(page)
+
+    });
 }catch(error){
     res.status(500).json({error:'Failed to fetch hospitals data'});
 }
